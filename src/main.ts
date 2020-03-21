231b7e16a462cfa1eab451f7c51fa2f1eba388ad
Michael <michael@mbp-van-michael.home>
2020-03-21 22:18:35 +0100

    added cors to api

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors(
+    {
+      origin: true,
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      allowedHeaders: 'Content-Type, Accept'
+    }
+  )
+
   await app.listen(3000);
 }
 bootstrap();
